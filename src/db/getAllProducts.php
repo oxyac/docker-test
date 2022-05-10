f79209d187efe25a7135be83872db8e35a4ee9af
Oleg Ghimp <ghimpco@gmail.com>
2022-05-10 08:31:39 +0300

    symfony init

--- src/db/getAllProducts.php
+++ src/db/getAllProducts.php
@@ -3,17 +3,26 @@
         public $id;
         public $name;
     }
-
     $products = array();
-    $dbc = mysqli_connect('localhost:3306', 'root', 'password', 'users');
+
+$servername = "users-mysql";
+$username = "og";
+$password = "password";
+$dbname = "users";
+$port = "3306";
+
+//$dbc = mysqli_connect('users_mysql:3306', 'og', 'password', 'users');
+//die;
+    $dbh = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
+//    die;
     $query = "SELECT id AS id_prod, name AS name_prod FROM products";
-    $data = mysqli_query($dbc, $query);
+    $data = $dbh->query($query);
    while($row = mysqli_fetch_assoc($data)){
         $product = new Product();
         $product->id = $row['id_prod'];
         $product->name = $row['name_prod'];
         array_push($products, $product);
    }
-   mysqli_close($dbc);
+   $dbh = null;
    echo json_encode($products);
 ?>
